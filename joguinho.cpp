bf61f8fc684e5703810c568c7493212fc1ea4cda
André <andrejuniorlopes@gmail.com>
2020-03-26 04:19:23 -0300

    O jogo agora possui o menu com as opções iniciar e sair funcionando. As imagens de seleção foram adicionadas aos arquivos

--- joguinho.cpp
+++ joguinho.cpp
@@ -34,15 +34,15 @@ int atirou = 0;
 float alfa=0;
 float incrementoX;
 float decrementoY;
-GLuint imagemIniciar;
+GLuint imagemIniciar,imagemIniciarSel;
 GLuint imagemSair;
 GLuint imagemPause;
 GLuint imagemReset;
-GLuint imagemSairMenu;
-GLuint imagemOpcoes;
-GLuint imagemDificil;
-GLuint imagemInstrucao;
-GLuint imagemCredito;
+GLuint imagemSairMenu,imagemSairMenuSel;
+GLuint imagemOpcoes,imagemOpcoesSel;
+GLuint imagemDificuldade,imagemDificuldadeSel;
+GLuint imagemInstrucao,imagemInstrucaoSel;
+GLuint imagemCredito,imagemCreditoSel;
 GLuint imagemLogo;
 GLuint longTime;
 Mix_Chunk *tiro;
@@ -51,6 +51,7 @@ Mix_Music *musicaMenu;
 enum telas {SPLASH, MENU, OPCOES, CREDITOS, JOJINHO} TELAS;
 int telaAtual=0;
 int aux=1;
+int selecao=1;
 float logoYMenu=800;
 float logoXtamanho=640;
 float logoYtamanho=320;
@@ -91,7 +92,7 @@ void resetar(){
 	if(movimentoInimigos<0)
 		movimentoInimigos*=-1;
 }
-
+//Função que verifica se os inimigos chegaram na borda da parte jogável da tela
 void verificaPosicao(){
 	for(int i=0;i<inimigos.size();i++){
 		if(inimigos[i].posicaoX>=1720){
@@ -110,13 +111,17 @@ void verificaPosicao(){
 		}
 	}
 }
-
+//Função que troca o valor da tela pelo valor recebido
+void trocaTela(int valor){
+	telaAtual=valor;
+}
+//Função que muda a posição das naves inimigas
 void moverNaves(){
 	for(int i=0;i<inimigos.size();i++){
 		inimigos[i].posicaoX+=movimentoInimigos;
 	}
 }
-
+//Função que inicializa o vector de inimigos e dá uma posição para cada um
 void iniciarInimigos(){
 	incrementoX=0;
 	decrementoY=0;
@@ -136,32 +141,32 @@ void iniciarInimigos(){
 		}
 	}
 }
-
+//Função que checa colisão entre as balas atiradas pelo jogador e as naves inimigas. Método AABB
 GLboolean checarColisao(Enemies enemy, Bullet bala){
 	bool colisaoX = (enemy.posicaoX + (enemy.larg)/2 >= (bala.x - 2)) && (bala.x + 2 >= enemy.posicaoX - (enemy.larg)/2);
     bool colisaoY = (enemy.posicaoY + (enemy.alt)/2 >= (bala.y - 15)) && (bala.y + 15 >= enemy.posicaoY - (enemy.alt)/2);
 	return colisaoX && colisaoY;
 }
-
+//Função que checa colisão entre as balas atiradas pelos inimigos e o jogador. Método AABB
 GLboolean checarColisaoPlayerBala(Player player, Bullet bala){
-	bool colisaoX = (player.posicaoX + (player.larg)/2 >= bala.x - 2) && (bala.x + 2 >= player.posicaoX - (bala.x)/2);
-    bool colisaoY = (player.posicaoY + (player.alt)/2 >= bala.y - 15) && (bala.y + 15 >= player.posicaoY - (bala.y)/2);
+	bool colisaoX = (player.posicaoX + (player.larg)/2 >= bala.x - 2) && (bala.x + 2 >= player.posicaoX - (player.larg/2));
+    bool colisaoY = (player.posicaoY + (player.alt)/2 >= bala.y - 15) && (bala.y + 15 >= player.posicaoY - (player.alt/2));
 	return colisaoX && colisaoY;
 }
-
+//Função que checa colisão entre as naves inimigas e o jogador. Método AABB
 GLboolean checarColisaoPlayerNaves(Player player, Enemies enemy){
 	bool colisaoX = (player.posicaoX + (player.larg)/2 >= enemy.posicaoX - (enemy.larg)/2) && (enemy.posicaoX + (enemy.larg)/2 >= player.posicaoX - (enemy.larg)/2);
     bool colisaoY = (player.posicaoY + (player.alt)/2 >= enemy.posicaoY - (enemy.alt)/2) && (enemy.posicaoY + (enemy.larg)/2 >= player.posicaoY - (enemy.alt)/2);
 	return colisaoX && colisaoY;
 }
-
+//Função que muda a posição do jogador
 void mover(){
 	if(direita==1 && jogador.posicaoX<1700)
 		jogador.posicaoX+=movimentoJogador;
 	if(esquerda==1 && jogador.posicaoX>200)
 		jogador.posicaoX-=movimentoJogador;
 }
-
+//Função de checagem de vitória
 GLboolean checarVitoria(){
 	if(inimigos.size()==0)
 		return true;
@@ -183,8 +188,8 @@ void iniciarJogador(){
 	jogador.vida=2;
 	jogador.textura = carregaTexturas("mfalcon.png");
 }
-
-void desenharRetanguloTextura(float x, float y, float larg, float alt, GLuint textura){
+//Função que desenha um retângulo dados a sua posição no eixo X e no eixo Y, sua largura, sua altura e a textura a ser utilizada. Textura estática
+void desenhaTexturaEstatica(float x, float y, float larg, float alt, GLuint textura){
 	glEnable(GL_TEXTURE_2D);
     glBindTexture(GL_TEXTURE_2D, textura);
     glPushMatrix();
@@ -199,7 +204,14 @@ void desenharRetanguloTextura(float x, float y, float larg, float alt, GLuint te
     glDisable(GL_TEXTURE_2D);
     glFlush();
 }
-
+//Função que muda a música de acordo com a tela na qual o jogador se encontra
+void tocaMusica(){
+	if(telaAtual==1 && !Mix_PlayingMusic())
+		Mix_PlayMusic(musicaMenu,-1);
+	else if(telaAtual==4 && !Mix_PlayingMusic())
+		Mix_PlayMusic(musicaBatalha,-1);
+}
+//Função que inicializa as texturas do menu
 void iniciarTexturas(){
 	longTime = carregaTexturas("a-long-time.png");
 	imagemLogo = carregaTexturas("star-wars-logo.png");
@@ -211,13 +223,21 @@ void iniciarTexturas(){
 	imagemReset = carregaTexturas("reset.png");
 	imagemSairMenu = carregaTexturas("sairMenu.png");
 	imagemCredito = carregaTexturas("creditos.png");
-	imagemDificil = carregaTexturas("dificuldade.png");
-}
+	imagemDificuldade = carregaTexturas("dificuldade.png");
 
+	imagemIniciarSel = carregaTexturas("iniciarSel.png");
+	imagemOpcoesSel = carregaTexturas("opcoesSel.png");
+	imagemDificuldadeSel = carregaTexturas("dificuldadeSel.png");
+	imagemCreditoSel = carregaTexturas("creditos.png");
+	imagemSairMenuSel = carregaTexturas("sairMenuSel.png");
+	imagemInstrucaoSel = carregaTexturas("instrucoesSel.png");
+}
+//Função que inicializa o jogo no geral
 void setup(){
 	glClearColor(0,0,0,0);
 	biri.push_back(2500);
 	biri.push_back(1800);
+	biri.push_back(400);
 	Mix_OpenAudio(44100,MIX_DEFAULT_FORMAT,2,4096);
 	musicaBatalha=Mix_LoadMUS("BattleOfTheHeroes.mp3");
 	musicaMenu=Mix_LoadMUS("Abertura8bit.mp3");
@@ -242,64 +262,86 @@ void draw(){
 	glClear(GL_COLOR_BUFFER_BIT);
 	glColor4f(1,1,1,alfa);
 	if(telaAtual==0){
-		desenharRetanguloTextura(960,540,800,412,longTime);
+		desenhaTexturaEstatica(960,540,800,412,longTime);
 	}
 	if(telaAtual==1){
-		desenharRetanguloTextura(960,800,640,320,imagemLogo);
-		desenharRetanguloTextura(960,540,103,33,imagemIniciar);
-		desenharRetanguloTextura(960,500,115,33,imagemOpcoes);
-		desenharRetanguloTextura(960,460,180,33,imagemInstrucao);
-		desenharRetanguloTextura(960,420,95,33,imagemSairMenu);
+		if(sair==1)
+			desenhaTexturaEstatica(960,540,960,540,imagemSair);
+		else{
+			desenhaTexturaEstatica(960,biri[2],biri[0],biri[1],imagemLogo);
+			/*A variável aux serve para verificar se a introdução do jogo já foi ou não finalizada para que se possa desenhas
+			as opções do menu*/
+			if(aux==1){
+				if(selecao==1)
+					desenhaTexturaEstatica(960,540,103,33,imagemIniciarSel);
+				else
+					desenhaTexturaEstatica(960,540,103,33,imagemIniciar);
+				if(selecao==2)
+					desenhaTexturaEstatica(960,500,115,33,imagemOpcoesSel);
+				else
+					desenhaTexturaEstatica(960,500,115,33,imagemOpcoes);
+				if(selecao==3)
+					desenhaTexturaEstatica(960,460,180,33,imagemInstrucaoSel);
+				else
+					desenhaTexturaEstatica(960,460,180,33,imagemInstrucao);
+				if(selecao==4)
+					desenhaTexturaEstatica(960,420,95,33,imagemSairMenuSel);
+				else
+					desenhaTexturaEstatica(960,420,95,33,imagemSairMenu);
+			}
+		}
 	}
 	if(telaAtual==4){
 		if(sair==0 && reset==0 && pausa==0){
-			desenharRetanguloTextura(jogador.posicaoX,jogador.posicaoY,jogador.larg,jogador.alt,jogador.textura);
+			desenhaTexturaEstatica(jogador.posicaoX,jogador.posicaoY,jogador.larg,jogador.alt,jogador.textura);
 			for(int i=0;i<inimigos.size();i++){
-				desenharRetanguloTextura(inimigos[i].posicaoX,inimigos[i].posicaoY,inimigos[i].larg,inimigos[i].alt,inimigos[i].textura);
+				desenhaTexturaEstatica(inimigos[i].posicaoX,inimigos[i].posicaoY,inimigos[i].larg,inimigos[i].alt,inimigos[i].textura);
 			}
 			desenhaTiro();
 			desenhaTiroInimigo();
 		}	
 		if(sair==1 && reset==0 && pausa==0){
 			Mix_PauseMusic();
-			desenharRetanguloTextura(960,540,960,540,imagemSair);
+			desenhaTexturaEstatica(960,540,960,540,imagemSair);
 		}
 		if(sair==0 && reset==1 && pausa==0){
 			Mix_PauseMusic();
-			desenharRetanguloTextura(960,540,960,540,imagemReset);
+			desenhaTexturaEstatica(960,540,960,540,imagemReset);
 		}
 		if(sair==0 && reset==0 && pausa==1){
 			Mix_PauseMusic();
-			desenharRetanguloTextura(960,540,960,540,imagemPause);
+			desenhaTexturaEstatica(960,540,960,540,imagemPause);
 		}
 	}
 	glutSwapBuffers();
 }
-
+//Função que verifica se o player pode ou não atirar. O tempo muda de acordo com a dificuldade
 void trocaValorAtira(int x){
 	if(podeAtirar)
 		podeAtirar = false;
 	else
 		podeAtirar = true;
 }
-
+//Cria uma bala do jogador
 void atira(int x, int y){
 		Bullet bullet;
 		bullet.x = x;
 		bullet.y = y;
 		bullets.push_back(bullet);
 }
-
+//Cria uma bala das naves inimigas
 void navesAtirar(int valor){
-	srand(time(0));
-	Bullet enemyBullet;
-	valor = rand()%inimigos.size();
-	enemyBullet.x = inimigos[valor].posicaoX;
-	enemyBullet.y = inimigos[valor].posicaoY;
-	enemyBullets.push_back(enemyBullet);
-	glutTimerFunc(5000,navesAtirar,0);
+	if(telaAtual==4){
+		srand(time(0));
+		Bullet enemyBullet;
+		valor = rand()%inimigos.size();
+		enemyBullet.x = inimigos[valor].posicaoX;
+		enemyBullet.y = inimigos[valor].posicaoY;
+		enemyBullets.push_back(enemyBullet);
+		glutTimerFunc(5000,navesAtirar,0);
+	}
 }
-
+//Desenha um tiro. Se a posição do tiro ultrapassar a altura máxima da tela, o tiro é apagado da memória
 void desenhaTiro(){
 	for(int i = 0; i < bullets.size();i++){
 		if(bullets[i].y>= 1080)
@@ -319,7 +361,7 @@ void desenhaTiro(){
 	   bullets[i].y+=25;
 	}
 }
-
+//Mesma coisa da função de cima, porém para as naves inimigas
 void desenhaTiroInimigo(){
 	for(int i = 0; i < enemyBullets.size();i++){
 		if(enemyBullets[i].y<=0)
@@ -370,6 +412,17 @@ void keyboard(unsigned char key, int x, int y){
 			else if(sair==0 && reset==1 && pausa==0){
 				reset=0;
 				Mix_ResumeMusic();
+			}
+			break;
+		case 13:
+			if(selecao==1 && aux==1){
+				Mix_HaltMusic();
+				trocaTela(4);
+				glutTimerFunc(5000,navesAtirar,0);
+				tocaMusica();
+			}
+			else if(selecao==4 && aux==1){
+				sair=1;
 			}
 			break;
 		case 27:
@@ -428,6 +481,16 @@ void specialKeyboard(int key, int x, int y){
 		case GLUT_KEY_RIGHT:
 			direita=1;
 			break;
+		case GLUT_KEY_UP:
+			if(telaAtual==1 && selecao>1){
+				selecao--;
+			}
+			break;
+		case GLUT_KEY_DOWN:
+			if(telaAtual==1 && selecao<4){
+				selecao++;
+			}
+			break;
 	}
 }
 
@@ -447,22 +510,36 @@ void atualizaCena(int tempo){
 		if(aux!=0){
 			if(alfa<1)
 				alfa+=0.01;
-			printf("%f\n",alfa);
 		}
 		else{
 			if(alfa>0)
 				alfa-=0.01;
+			else{
+				alfa=1;
+				trocaTela(1);
+			}
 		}
 		if(alfa>=1){
 			aux=0;
 			alfa=0.9999;
-			sleep(4);
+			sleep(2);
 		}
 	}
+	//Aqui é verificado se a logo já ficou no tamanho e na posição já predefinidas para o menu
 	if(telaAtual==1){
-		alfa=1;
+		tocaMusica();
+		if(biri[2]<800)
+			biri[2]+=1.1;
+		if(biri[0]>640)
+			biri[0]-=5.0;
+		if(biri[1]>320)
+			biri[1]-=4.0;
+		else if(biri[2]>=800 && biri[0]<=640 && biri[1]<=320)
+			aux=1;
 	}
+	//Aqui fica a lógica do jogo
 	if(telaAtual==4){
+		tocaMusica();
 		for(int i=0;i<inimigos.size();i++){
 			if(checarColisaoPlayerNaves(jogador,inimigos[i]))
 				exit(0);
@@ -478,11 +555,17 @@ void atualizaCena(int tempo){
 				enemyBullets.erase(enemyBullets.begin()+i);
 				jogador.vida--;
 			}
-			if(jogador.vida==0)
+			if(jogador.vida==0){
+				printf("perdeu\n");
 				exit(0);
+			}
 		}
-		if(checarVitoria())
-			exit(0); // aqui vai ficar a tela de vitória e td mais
+		if(checarVitoria()){
+			Mix_HaltMusic();
+			telaAtual=1;
+			tocaMusica();
+			resetar(); // aqui vai ficar a tela de vitória e td mais
+		}
 		if(sair==0 && reset==0 && pausa==0){
 			mover();
 			verificaPosicao();
@@ -497,22 +580,17 @@ int main(int argc, char **argv){
 	glutInit(&argc, argv);
 	SDL_Init(SDL_INIT_AUDIO);
 	Mix_Init(MIX_INIT_MP3);
-
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
 	glutInitWindowPosition(0,0);
-
 	glutInitWindowSize(1920,1080);
 	glutCreateWindow("Star Wars - the game");
 	setup();
-
 	glutDisplayFunc(draw);
 	glutReshapeFunc(reshape);
 	glutKeyboardFunc(keyboard);
 	glutKeyboardUpFunc(keyboardUp);
 	glutSpecialUpFunc(specialKeyboardUp);
 	glutSpecialFunc(specialKeyboard);
-	glutTimerFunc(5000,navesAtirar,0);
 	glutTimerFunc(0,atualizaCena,33);
-
 	glutMainLoop();
 }
